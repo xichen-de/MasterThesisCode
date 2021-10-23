9a52ca81d2800e0df31b8915ef3930ed90ae867e
Xi Chen <52621857+xichen-de@users.noreply.github.com>
2021-10-23 12:24:32 +0200

    Change license to MIT.

--- src/tools/visualization/get_test_results.py
+++ src/tools/visualization/get_test_results.py
@@ -8,6 +8,16 @@ folder and be named like this: name (same) + experiment_id.
 Therefore, after we extract the test results, we put each test result in separate folder with the name:
 log_folder_name + experiment id, for example: DDPG_PLAN-0, DDPG_PLAN-1, etc.
 """
+#  MIT License
+#
+#  Copyright 2021 Xi Chen
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import argparse
 import glob
 import os
